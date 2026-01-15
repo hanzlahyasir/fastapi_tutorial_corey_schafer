5b34ea20c77c4de29707289607eaa0b4d62491cb
hanzlahyasir <hanzlahyasir@gmail.com>
2026-01-15 14:48:35 +0500

    Added HTML and CSS to Layout and Home html files

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
@@ -22,10 +24,10 @@ posts: list[dict] = [
     },
 ]
 
-@app.get("/", include_in_schema=False)
-@app.get("/posts", include_in_schema=False)
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name='posts')
 def home(request: Request):
-    return templates.TemplateResponse(request, 'home.html', {'posts': posts})
+    return templates.TemplateResponse(request, 'home.html', {'posts': posts, 'title': 'Home'})
 
 
 @app.get("/api/posts")
